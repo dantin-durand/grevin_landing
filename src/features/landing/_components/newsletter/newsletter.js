b2b3d574de314fa8fc18f0fcf41ae99cb70ce60c
Viezas <dxviezas@gmail.com>
2022-05-08 23:32:41 +0200

    Fix font-size for input

--- src/features/landing/_components/newsletter/newsletter.js
+++ src/features/landing/_components/newsletter/newsletter.js
@@ -5,7 +5,7 @@ export default function Newsletter() {
                 <h3 className="text-md text-center mb-4">Abonnez-vous à notre newsletter:</h3>
                 <p>Ne ratez aucune nouvelle personalité de notre musée !</p>
                 <div className="grid grid-rows-1 grid-cols-12 my-4">
-                    <input className="col-start-2 col-span-10 rounded-full pl-6 py-2 mb-4 text-md" type="text" placeholder="Adresse mail..."/>
+                    <input className="col-start-2 col-span-10 rounded-full pl-6 py-2 mb-4 text-sm text-black" type="email" placeholder="Adresse mail..."/>
                     <button className="col-start-7 col-span-5 flex items-center justify-between rounded-full bg-yellow text-red text-sm px-4 py-1">
                         <img src="/assets/icons/mail.svg" style={{height: '1.5em'}}/>
                         S'abonner
