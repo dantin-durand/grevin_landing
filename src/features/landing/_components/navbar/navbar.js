15241a7a0ffad77c9a6117b0a4c5c027fb50a856
dantin-durand <durand.dantin.pro@gmail.com>
2022-05-15 14:20:30 +0200

    [func] add navbar component

--- src/features/landing/_components/navbar/navbar.js
+++ src/features/landing/_components/navbar/navbar.js
@@ -1,3 +1,6 @@
+import joinClasses from "helpers/joinClasses";
+import navbarItems from "utils/navbar";
+
 export default function Navbar() {
   return (
     <nav className="flex flex-col">
@@ -9,10 +12,31 @@ export default function Navbar() {
         <a href="/">
           <img src="/assets/logo/logo-white.svg" alt="logo" />
         </a>
-        <button className="flex gap-2 text-white items-center">
-          <img src="/assets/icons/shopping-bag.svg" />
+        <button className="flex gap-2 text-white items-center border-red border-s p-default rounded-full">
+          <img
+            src="/assets/icons/shopping-bag.svg"
+            alt=""
+            className="w-[18px]"
+          />
           <p>Boutique</p>
         </button>
+      </div>
+      <div className="h-[130px] flex flex-col bg-black justify-end px-10">
+        <div className="w-full h-[60px] border-t border-white flex justify-center items-center">
+          <ul className="flex justify-center text-white items-center gap-10 text-[20px] uppercase font-medium">
+            {navbarItems.map((item) => (
+              <li key={item.name} className="group flex flex-col items-center">
+                <a href={item.path}>{item.name}</a>
+                <div
+                  className={joinClasses(
+                    "h-[2px] w-0 bg-white transition-all group-hover:w-full",
+                    item.active && "w-[32px]"
+                  )}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
---
+++ src/utils/navbar.js
@@ -0,0 +1,19 @@
+const navbarItems = [
+  {
+    name: "Accueil",
+    path: "/",
+    active: true,
+  },
+  {
+    name: "Découvrir",
+    path: "/",
+    active: false,
+  },
+  {
+    name: "En ce moment",
+    path: "/",
+    active: false,
+  },
+];
+
+export default navbarItems;
